b11f9c6e69735eb616c3531bde77331bfcb8eab1
taktazit <taktazit@outlook.com>
2017-05-31 15:12:03 +0430

    extractManagedObjects functionality added. It will helpful for extracting and deserializing managedObjects from Raml file.

--- src/main/java/co/brtel/licenseutilizationcalculator/parsers/RamlSerializer.java
+++ src/main/java/co/brtel/licenseutilizationcalculator/parsers/RamlSerializer.java
@@ -1,13 +1,89 @@
 package co.brtel.licenseutilizationcalculator.parsers;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.xml.bind.JAXBException;
 
 import co.brtel.licenseutilizationcalculator.commons.Serializer;
+import co.brtel.licenseutilizationcalculator.pojo.ManagedObject;
 import co.brtel.licenseutilizationcalculator.pojo.Raml;
 
 public class RamlSerializer {
-	public static Raml deserialize(String xml) throws JAXBException{
+	private static final String RAML_HEAD_TEMPLATE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><raml version=\"2.0\"><cmData type=\"actual\">";
+	private static final String RAML_TAIL_TEMPLATE = "</cmData></raml>";
+	private static final String MANAGED_OBJECT_CLOSING_TAG = "</managedObject>";
+
+	public static Raml deserialize(String xml) throws JAXBException {
 		Serializer<Raml> serializer = new Serializer<Raml>(new Raml());
 		return serializer.deserialize(xml);
+	}
+
+	/**
+	 * Extracts all ManagedObjects inside XML files. This function is useful for extracting objects in multiple files and when files are big.
+	 * @param filePathes
+	 * @return
+	 * @throws JAXBException
+	 * @throws UnsupportedEncodingException
+	 * @throws IOException
+	 */
+	public static List<ManagedObject> extractManagedObjects(List<String> filePathes) throws JAXBException, UnsupportedEncodingException, IOException {
+		int progress = 0;
+		int page = 20000;
+		List<ManagedObject> managedObjects = new LinkedList<ManagedObject>();
+		StringBuilder sb = new StringBuilder();
+
+		for (String xmlStream : filePathes) {
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(xmlStream)), "UTF-8"))) {
+				skipUnusedRamlParts(br);
+
+				String line;
+				while ((line = br.readLine()) != null) {
+					sb.append(line);
+					sb.append("\r\n");
+					progress++;
+					if (progress % page == 0) {
+						if (!line.contains(MANAGED_OBJECT_CLOSING_TAG)) {
+							line = br.readLine();
+							while (!line.contains(MANAGED_OBJECT_CLOSING_TAG)) {
+								sb.append(line);
+								sb.append("\r\n");
+								progress++;
+								line = br.readLine();
+							}
+							sb.append(line);
+							sb.append("\r\n");
+							progress++;
+
+							managedObjects.addAll(Arrays.asList(deserialize(RAML_HEAD_TEMPLATE + sb.toString() + RAML_TAIL_TEMPLATE).getCmData().getManagedObject()));
+							sb = new StringBuilder();
+							progress = 0;
+						}
+					}
+				}
+				if (progress > 0) {
+					managedObjects.addAll(Arrays.asList(deserialize(RAML_HEAD_TEMPLATE + sb.toString().replace("</cmData>", "").replace("</raml>", "") + RAML_TAIL_TEMPLATE)
+							.getCmData().getManagedObject()));
+					sb = new StringBuilder();
+					progress = 0;
+				}
+			}
+		}
+
+		return managedObjects;
+	}
+
+	private static void skipUnusedRamlParts(BufferedReader br) throws IOException {
+		br.readLine();
+		br.readLine();
+		br.readLine();
+		br.readLine();
 	}
 }
--- src/test/java/co/brtel/licenseutilizationcalculator/parsers/RamlSerializerTests.java
+++ src/test/java/co/brtel/licenseutilizationcalculator/parsers/RamlSerializerTests.java
@@ -1,8 +1,12 @@
 package co.brtel.licenseutilizationcalculator.parsers;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.xml.bind.JAXBException;
@@ -11,12 +15,14 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import co.brtel.licenseutilizationcalculator.commons.Serializer;
+import co.brtel.licenseutilizationcalculator.pojo.ManagedObject;
 import co.brtel.licenseutilizationcalculator.pojo.Raml;
 
 public class RamlSerializerTests {
 	private String ramlSingleManagedObjectSampleXml;
 	private String ramlMultipleManagedObjectSampleXml;
+	private String ramlHugeNumberOfManagedObjectSampleXml1;
+	private String ramlHugeNumberOfManagedObjectSampleXml2;
 
 	@Before
 	public void setUp() throws IOException {
@@ -24,6 +30,9 @@ public class RamlSerializerTests {
 				Files.lines(new File(getClass().getResource("/singleRamlSampleXml.xml").getFile()).toPath()).collect(Collectors.toList()));
 		ramlMultipleManagedObjectSampleXml = String.join("\r\n",
 				Files.lines(new File(getClass().getResource("/multipleRamlSampleXml.xml").getFile()).toPath()).collect(Collectors.toList()));
+		
+		ramlHugeNumberOfManagedObjectSampleXml1 = getClass().getResource("/hugeDump1.xml").getFile();
+		ramlHugeNumberOfManagedObjectSampleXml2 = getClass().getResource("/hugeDump2.xml").getFile();
 	}
 
 	@Test
@@ -37,8 +46,18 @@ public class RamlSerializerTests {
 	public void testRamlWithMultipleManagedObjectDeserialization() throws JAXBException {
 		Raml raml = RamlSerializer.deserialize(ramlMultipleManagedObjectSampleXml);
 
-		Assert.assertEquals(1950, raml.getCmData().getManagedObject().length);
+		Assert.assertEquals(2, raml.getCmData().getManagedObject().length);
 		testRamlContent(raml);
+	}
+	
+	@Test
+	public void testRamlWithHugeNumberOfManagedObjectDeserialization() throws JAXBException, UnsupportedEncodingException, IOException {
+		List<ManagedObject> managedObjects = RamlSerializer.extractManagedObjects(Arrays.asList(ramlHugeNumberOfManagedObjectSampleXml1, ramlHugeNumberOfManagedObjectSampleXml2));
+		long rnfcCount = managedObjects.stream().filter(item -> item.getClassName().equals("RNC")).count();
+		long ipbrCount = managedObjects.stream().filter(item -> item.getClassName().equals("IPBR")).count();
+		
+		Assert.assertEquals(13, rnfcCount);
+		Assert.assertEquals(62, ipbrCount);
 	}
 
 	private void testRamlContent(Raml raml) {
