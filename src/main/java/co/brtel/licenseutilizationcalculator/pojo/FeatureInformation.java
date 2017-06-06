54fa34f994d5e0b0c6eb9a3794a4c2494808fbf3
taktazit <taktazit@outlook.com>
2017-06-06 16:52:07 +0430

    ManagedObjectType added

--- src/main/java/co/brtel/licenseutilizationcalculator/pojo/FeatureInformation.java
+++ src/main/java/co/brtel/licenseutilizationcalculator/pojo/FeatureInformation.java
@@ -119,6 +119,112 @@ public class FeatureInformation {
 		aMap.put("4839", CapacityUnit.CELL);
 		capacityUnitMap = Collections.unmodifiableMap(aMap);
 	}
+	
+	private static final Map<String, ManagedObjectType> managedObjectTypeMap;
+	static {
+		Map<String, ManagedObjectType> aMap = new HashMap<>();
+		aMap.put("619", ManagedObjectType.NONE);
+		aMap.put("620", ManagedObjectType.NONE);
+		aMap.put("621", ManagedObjectType.NONE);
+		aMap.put("622", ManagedObjectType.NONE);
+		aMap.put("623", ManagedObjectType.NONE);
+		aMap.put("625", ManagedObjectType.NONE);
+		aMap.put("627", ManagedObjectType.RNFC);
+		aMap.put("628", ManagedObjectType.RNMOBI);
+		aMap.put("630", ManagedObjectType.RNFC);
+		aMap.put("633", ManagedObjectType.WBTS);
+		aMap.put("638", ManagedObjectType.WCEL);
+		aMap.put("641", ManagedObjectType.WBTS);
+		aMap.put("811", ManagedObjectType.COMMAND);
+		aMap.put("885", ManagedObjectType.RNFC);
+		aMap.put("958", ManagedObjectType.COMMAND);
+		aMap.put("959", ManagedObjectType.COMMAND);
+		aMap.put("960", ManagedObjectType.COMMAND);
+		aMap.put("961", ManagedObjectType.WCEL);
+		aMap.put("962", ManagedObjectType.WCEL);
+		aMap.put("966", ManagedObjectType.WCEL);
+		aMap.put("967", ManagedObjectType.RNFC);
+		aMap.put("968", ManagedObjectType.RNFC);
+		aMap.put("969", ManagedObjectType.NONE);
+		aMap.put("974", ManagedObjectType.NONE);
+		aMap.put("975", ManagedObjectType.RNFC);
+		aMap.put("1028", ManagedObjectType.RNHSPA);
+		aMap.put("1029", ManagedObjectType.NONE);
+		aMap.put("1030", ManagedObjectType.NONE);
+		aMap.put("1057", ManagedObjectType.NONE);
+		aMap.put("1060", ManagedObjectType.RNFC);
+		aMap.put("1061", ManagedObjectType.RNFC);
+		aMap.put("1062", ManagedObjectType.RNFC);
+		aMap.put("1069", ManagedObjectType.NONE);
+		aMap.put("1070", ManagedObjectType.NONE);
+		aMap.put("1071", ManagedObjectType.NONE);
+		aMap.put("1079", ManagedObjectType.NONE);
+		aMap.put("1080", ManagedObjectType.FMCS);
+		aMap.put("1085", ManagedObjectType.WBTS);
+		aMap.put("1086", ManagedObjectType.WCEL);
+		aMap.put("1087", ManagedObjectType.WCEL);
+		aMap.put("1088", ManagedObjectType.WBTS);
+		aMap.put("1089", ManagedObjectType.WCEL);
+		aMap.put("1091", ManagedObjectType.WCEL);
+		aMap.put("1092", ManagedObjectType.WBTS);
+		aMap.put("1093", ManagedObjectType.WBTS);
+		aMap.put("1107", ManagedObjectType.NONE);
+		aMap.put("1108", ManagedObjectType.NONE);
+		aMap.put("1109", ManagedObjectType.FMCS);
+		aMap.put("1110", ManagedObjectType.NONE);
+		aMap.put("1246", ManagedObjectType.NONE);
+		aMap.put("1247", ManagedObjectType.WBTS);
+		aMap.put("1279", ManagedObjectType.WBTS);
+		aMap.put("1302", ManagedObjectType.FMCG);
+		aMap.put("1303", ManagedObjectType.WBTS);
+		aMap.put("1305", ManagedObjectType.NONE);
+		aMap.put("1306", ManagedObjectType.COMMAND);
+		aMap.put("1375", ManagedObjectType.RNC);
+		aMap.put("1434", ManagedObjectType.RNC);
+		aMap.put("1435", ManagedObjectType.NONE);
+		aMap.put("1471", ManagedObjectType.WCEL);
+		aMap.put("1475", ManagedObjectType.WCEL);
+		aMap.put("1476", ManagedObjectType.WCEL);
+		aMap.put("1478", ManagedObjectType.NONE);
+		aMap.put("1485", ManagedObjectType.WCEL);
+		aMap.put("1490", ManagedObjectType.NONE);
+		aMap.put("1497", ManagedObjectType.WCEL);
+		aMap.put("1683", ManagedObjectType.NONE);
+		aMap.put("1684", ManagedObjectType.COMMAND_ZQRL);
+		aMap.put("1686", ManagedObjectType.RNFC);
+		aMap.put("1690", ManagedObjectType.RNFC);
+		aMap.put("1754", ManagedObjectType.COMMAND);
+		aMap.put("1755", ManagedObjectType.WCEL);
+		aMap.put("1756", ManagedObjectType.WCEL);
+		aMap.put("1795", ManagedObjectType.WCEL);
+		aMap.put("1796", ManagedObjectType.NONE);
+		aMap.put("1798", ManagedObjectType.WCEL);
+		aMap.put("1897", ManagedObjectType.NONE);
+		aMap.put("1898", ManagedObjectType.NONE);
+		aMap.put("1938", ManagedObjectType.NONE);
+		aMap.put("1939", ManagedObjectType.COMMAND);
+		aMap.put("2117", ManagedObjectType.NONE);
+		aMap.put("3384", ManagedObjectType.RNFC);
+		aMap.put("3414", ManagedObjectType.WCEL);
+		aMap.put("3420", ManagedObjectType.WCEL);
+		aMap.put("3422", ManagedObjectType.WCEL);
+		aMap.put("3898", ManagedObjectType.WCEL);
+		aMap.put("3903", ManagedObjectType.WCEL);
+		aMap.put("4160", ManagedObjectType.WCEL);
+		aMap.put("4290", ManagedObjectType.RNFC);
+		aMap.put("4293", ManagedObjectType.RNFC);
+		aMap.put("4348", ManagedObjectType.RNFC);
+		aMap.put("4492", ManagedObjectType.WCEL);
+		aMap.put("4504", ManagedObjectType.WCEL);
+		aMap.put("4533", ManagedObjectType.WCEL);
+		aMap.put("4538", ManagedObjectType.WCEL);
+		aMap.put("4545", ManagedObjectType.WCEL);
+		aMap.put("4780", ManagedObjectType.RNFC);
+		aMap.put("4783", ManagedObjectType.WBTS);
+		aMap.put("4839", ManagedObjectType.WCEL);
+		
+		managedObjectTypeMap = Collections.unmodifiableMap(aMap);
+	}
 
 	public RNC getRnc() {
 		return rnc;
@@ -170,5 +276,9 @@ public class FeatureInformation {
 
 	public CapacityUnit getCapacityUnit() {
 		return capacityUnitMap.get(code);
+	}
+	
+	public ManagedObjectType getManagedObjectType() {
+		return managedObjectTypeMap.get(code);
 	}
 }
---
+++ src/main/java/co/brtel/licenseutilizationcalculator/pojo/ManagedObjectType.java
@@ -0,0 +1,5 @@
+package co.brtel.licenseutilizationcalculator.pojo;
+
+public enum ManagedObjectType {
+	RNFC, RNMOBI, WBTS, WCEL, COMMAND, FMCS, FMCG, NONE, RNHSPA, RNC, COMMAND_ZQRL
+}
